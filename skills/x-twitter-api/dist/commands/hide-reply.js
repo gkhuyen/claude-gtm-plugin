280a4ab27b0010805d21c27c7a95bd97add47240
Manoj Bajaj <manojbajaj95@gmail.com>
2026-02-18 08:49:53 +0000

    Remove unnecessary skills

--- skills/x-twitter-api/dist/commands/hide-reply.js
+++
@@ -1,7 +0,0 @@
-import { parseArgs } from "../lib/args.js";
-export async function hideReply(client, args) {
-    const { tweetId } = parseArgs(args, {
-        positional: { key: "tweetId", label: "A tweet ID" },
-    });
-    return client.posts.hideReply(tweetId, { body: { hidden: true } });
-}
